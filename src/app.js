1beb5087e6be4dc9b62441a989cb128e6fce764c
Anthony Carzon <apjcarzon@gmail.com>
2024-03-12 13:39:19 -0400

    6th added tests & fizzbuzz in app.test.js & app.js

--- src/app.js
+++ src/app.js
@@ -1,3 +1,19 @@
 export function add(p1, p2) {
   return p1 + p2;
 }
+
+export function subtract(w1, w2) {
+  return w1 - w2;
+}
+
+export function multiply(t1, t2) {
+  return t1 * t2;
+}
+
+export function divide(g1, g2) {
+  return g1 / g2;
+}
+
+export function fizzbuzz(f) {
+  return f;
+}
--- src/app.test.js
+++ src/app.test.js
@@ -1,7 +1,19 @@
-import { add } from "./app";
+import { add, subtract, multiply, divide, fizzbuzz } from "./app";
 
 describe("my test suite", () => {
-  it("first test", () => {
+  it("adds 1 + 1", () => {
     expect(add(1, 1)).toEqual(2);
+  });
+  it("subtracts 2 - 1", () => {
+    expect(subtract(2, 1)).toEqual(1);
+  });
+  it("multiplies 2 * 2", () => {
+    expect(multiply(2, 2)).toEqual(4);
+  });
+  it("divide 6 / 2", () => {
+    expect(divide(6, 2)).toEqual(3);
+  });
+  it("fizzbuzz returns 1", () => {
+    expect(fizzbuzz(1)).toEqual(1);
   });
 });
